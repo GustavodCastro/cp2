ef46fa9b6678410353a55abd29250d02926670c0
Gustavo De Assis De Castro <02528778104@dccbra.ifb.local>
2024-04-08 20:55:29 -0300

    Aula CP2 08/04 - Eventos de Usuario, Componentes, Metodos. Uso de Emit e Props

--- vue/cli/src/components/ImagemComponent.vue
+++ vue/cli/src/components/ImagemComponent.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="container">
-        <img v-bind:src="urlImagem" alt="Banner">
-
+    <p>Nome da Imagem: {{ nomeImagem }}</p>
+    <img v-show="ImagemVisivel":src="urlImagem" alt="Banner">
+    <button @click="apareceEsconde(), segundoMetodo()">Aparece/Esconde</button>
     </div>
 
 
@@ -13,14 +14,48 @@
         name: "ImagemComponent",
         data(){
             return {
-                urlImagem : "https://shorturl.at/jxGTY"
+                ImagemVisivel : false,
+                //urlImagem : "https://shorturl.at/jxGTY"
         }
-    }
+    },
+    methods:{
+        apareceEsconde(){
+            console.log("Fui clicado!!");
+            this.ImagemVisivel = !this.ImagemVisivel;
+            this.$emit('visivelImg', this.ImagemVisivel); 
+
+        },
+        segundoMetodo(){
+            setTimeout(function(){
+                console.log("Segundo método chamado");
+            }, 2000);
+        }
+    },props:{
+        nomeImagem: String,
+        urlImagem : String,
+    },
+    emits: ["visivelImg"],
 }
 </script>
 
 
 <style scoped>
+    button{
+        padding: 12px;
+        font-weight: bold;
+        color:green;
+        background-color: azure;
+        border: solid 2px grey;
+        border-radius: 8px;
+        transition: 0.5s;
+        cursor: pointer;
+    }
+
+    button:hover{
+        background-color: darkcyan;
+        color: aliceblue;
+    }
+
      img {
         width: 300px;
         height: auto;
--- vue/cli/src/components/UsuarioComponent.vue
+++ vue/cli/src/components/UsuarioComponent.vue
@@ -2,7 +2,16 @@
     <div>
         <p>Nome: {{ nome }}</p>
         <p>E-mail: {{ email }}</p>
-        <imagem-component/>
+        <imagem-component 
+        @visivel-img="exibirMsgZ"
+        nomeImagem="Imagem Ziraldo" 
+        url-imagem="https://static.sambafoot.com/wp/sites/2/GKgWuuTWcAAnUW4.jpeg"/>
+
+        <h1 v-show="isTimao">Timão é campeão!</h1>
+        
+        <imagem-component
+        nome-imagem="Timão"
+        url-imagem="https://s3.static.brasilescola.uol.com.br/be/2023/09/1-escudo-do-corinthians.jpg"/>
         
     </div>   
     
@@ -21,7 +30,17 @@ export default {
     data(){
         return {
             nome : "Gustavo",
-            email : "gustavo.castro@gmail.com"
+            email : "gustavo.castro@gmail.com",
+            isTimao : false
+        }
+    },
+    methods:{
+        exibirMsgZ(isImagemVisivel){
+            if(isImagemVisivel == true){
+                this.isTimao = true;
+            }else{
+                this.isTimao = false;
+            }
         }
     }
 }
